5cd49b4ea988d8883d3516b457136ea66cd73d37
Galagamage Umanda Thathsarani <guthathsarani@students.nsbm.lk>
2020-12-09 01:17:41 -0800

    Umanda

---
+++ MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView title;
+    private Button start,admin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        title = findViewById(R.id.main_title);
+        start = findViewById(R.id.ma_startB);
+        admin=  findViewById(R.id.admin);
+
+
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CatActivity.class);
+                startActivity(intent);
+            }
+        });
+        admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainAdminActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
