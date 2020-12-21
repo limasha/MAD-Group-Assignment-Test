e475e89202ee83ab2abfd8effb7249a68e8a4c5d
Galagamage Umanda Thathsarani <guthathsarani@students.nsbm.lk>
2020-12-20 22:41:44 -0800

    Umanda

--- SplashActivity.java
+++ SplashActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+
 public class SplashActivity extends AppCompatActivity {
     private TextView appName;
     @Override
