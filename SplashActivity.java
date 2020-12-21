79714d134b5d66150fb1d353d8dc060f2f549156
Galagamage Umanda Thathsarani <guthathsarani@students.nsbm.lk>
2020-12-20 22:49:35 -0800

    Umanda

--- SplashActivity.java
+++ SplashActivity.java
@@ -1,38 +1,95 @@
 package com.example.quizapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SplashActivity extends AppCompatActivity {
     private TextView appName;
+    public static List<CategoryModel> catList = new ArrayList<>();
+    public static int selected_cat_index = 0;
+    private FirebaseFirestore firestore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        appName = findViewById(R.id.appName);
+
+        firestore = FirebaseFirestore.getInstance();
+
         new Thread() {
             public void run() {
+                // sleep(3000);
 
-                try {
-                    sleep(3000);
-                }
-                catch (Exception e)
-                {
-
-                }
-
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(intent);
-                SplashActivity.this.finish();
+                loadData();
 
 
             }
         }.start();
+
+    }
+
+    private void loadData()
+    {
+        catList.clear();
+
+        firestore.collection("Quiz").document("Categories")
+                .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                if(task.isSuccessful())
+                {
+                    DocumentSnapshot doc = task.getResult();
+
+                    if(doc.exists())
+                    {
+                        long count = (long)doc.get("COUNT");
+
+                        for(int i=1; i <= count; i++)
+                        {
+                            String catName = doc.getString("CAT" + String.valueOf(i));
+                            String catID = doc.getString("CAT" + String.valueOf(i) + "_ID");
+
+                            catList.add(new CategoryModel(catID,catName));
+                        }
+
+
+                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        SplashActivity.this.finish();
+
+                    }
+                    else
+                    {
+                        Toast.makeText(SplashActivity.this,"No Category Document Exists!",Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+
+                }
+                else
+                {
+
+                    Toast.makeText(SplashActivity.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 }
