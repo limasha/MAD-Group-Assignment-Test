6a1cec1170a2aa6b6187327837f81fab98fb5ba1
Galagamage Umanda Thathsarani <guthathsarani@students.nsbm.lk>
2020-12-23 22:57:11 -0800

    Umanda

---
+++ CatGridAdapter.java
@@ -0,0 +1,75 @@
+package com.example.quizapp;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Random;
+
+public class CatGridAdapter extends BaseAdapter {
+
+    private List<CategoryModel> catList;
+
+    public CatGridAdapter(List<CategoryModel> catList) {
+        this.catList = catList;
+    }
+
+    @Override
+    public int getCount() {
+        return catList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, final ViewGroup parent) {
+
+
+        View view;
+
+        if(convertView == null)
+        {
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cat_item_layout,parent,false);
+        }
+        else
+        {
+            view = convertView;
+        }
+
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SplashActivity.selected_cat_index = position;
+                Intent intent = new Intent(parent.getContext(),SetActivity.class);
+                intent.putExtra("Catagories ", position);
+                intent.putExtra("CAT_ID", position+1);
+                parent.getContext().startActivity(intent);
+
+            }
+        });
+
+
+        ((TextView) view.findViewById(R.id.catName)).setText(catList.get(position).getName());
+
+        Random rnd = new Random();
+        int color = Color.argb(255, rnd.nextInt(255),rnd.nextInt(255),rnd.nextInt(255));
+        view.setBackgroundColor(color);
+
+
+        return view;
+    }
+}
