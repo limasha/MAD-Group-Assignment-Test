43bd50f4a86570c3797a297453a376c6ce517e9d
Galagamage Umanda Thathsarani <guthathsarani@students.nsbm.lk>
2020-12-23 22:56:46 -0800

    Umanda

---
+++ CategoryModel.java
@@ -0,0 +1,30 @@
+package com.example.quizapp;
+
+public class CategoryModel {
+
+    //private String id;
+    private String name;
+
+    public CategoryModel( String name) {
+        //this.id = id;
+        this.name = name;
+    }
+
+    /**public String getId() {
+        return id;
+    }
+
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+**/
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
