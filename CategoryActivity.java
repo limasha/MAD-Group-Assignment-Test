2bc00eea9817db01d77d91700946eddb4f8d6b22
Galagamage Umanda Thathsarani <guthathsarani@students.nsbm.lk>
2020-12-23 22:57:44 -0800

    Umanda

---
+++ CategoryActivity.java
@@ -0,0 +1,43 @@
+package com.example.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+
+import android.os.Bundle;
+import android.widget.GridView;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import static com.example.quizapp.SplashActivity.catList;
+
+
+public class CategoryActivity extends AppCompatActivity {
+    private GridView catGrid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+
+        getSupportActionBar().setTitle("Categories");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        catGrid = findViewById(R.id.catGridview);
+
+
+
+        CatGridAdapter adapter = new CatGridAdapter(catList);
+        catGrid.setAdapter(adapter);
+
+
+
+
+
+
+
+
+
+    }
+}
