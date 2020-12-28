5f071f0568bd804f12ef9ab47076341e8e7b0856
Galagamage Umanda Thathsarani <guthathsarani@students.nsbm.lk>
2020-12-28 06:47:49 -0800

    Umanda

--- MainActivity.java
+++ MainActivity.java
@@ -9,23 +9,35 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView appName;
-    private Button start;
+    private TextView title;
+    private Button start,admin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        appName = findViewById(R.id.AppName);
-        start = findViewById(R.id.start_button);
+
+        title = findViewById(R.id.main_title);
+        start = findViewById(R.id.ma_startB);
+        admin=  findViewById(R.id.admin);
+
+
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Category_activity_k.class);
+                Intent intent = new Intent(MainActivity.this, CategoryActivity.class);
                 startActivity(intent);
             }
         });
+        admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainAdminActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
